c073c6102afef76e6b1202f39232d10de76c9974
charlie la fosse <charlielafosse@outlook.com>
2018-10-19 16:38:13 +0100

    another typo

--- myJS.js
+++ myJS.js
@@ -6,11 +6,13 @@ function func(e){
 
 document.getElementById("keyboard").addEventListener("click", func2, false);
 
-// addition logic
+// ADDITION LOGIC
+// intialise accumulatingValue as 0
 // accumulate button clicks into a number
+// add currentValue to accumulatingValue when + clicked
 let displayNumb = [];
 function func2(t){
-  if(t.target.innerHTML.charCode > 47 || t.charCode < 58){
+  if(t.target.innerHTML.charCode > 47 || t.target.innerHTML.charCode < 58){
     console.log("you clicked a number");
     displayNumb.push(t.target.innerHTML);
     console.log(displayNumb);
