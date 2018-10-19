e9235d4ea052d7c68d1a2352516db2b2f843a937
charlie la fosse <charlielafosse@outlook.com>
2018-10-19 16:33:59 +0100

    typo

--- myJS.js
+++ myJS.js
@@ -10,7 +10,7 @@ document.getElementById("keyboard").addEventListener("click", func2, false);
 // accumulate button clicks into a number
 let displayNumb = [];
 function func2(t){
-  if(t.target.innerHTML.charCode > 47 || e.charCode < 58){
+  if(t.target.innerHTML.charCode > 47 || t.charCode < 58){
     console.log("you clicked a number");
     displayNumb.push(t.target.innerHTML);
     console.log(displayNumb);
