75adf1180c8416b29ef3fa01d4d851df9acf71f2
charlie la fosse <charlielafosse@outlook.com>
2018-10-19 16:53:13 +0100

    more console logging

--- myJS.js
+++ myJS.js
@@ -1,10 +1,8 @@
-function func(e){
-  if(e.target){
-    document.getElementById("display").innerHTML = e.target.innerHTML;
-  }
-}
-
-document.getElementById("keyboard").addEventListener("click", func2, false);
+// function func(e){
+//   if(e.target){
+//     document.getElementById("display").innerHTML = e.target.innerHTML;
+//   }
+// }
 
 // ADDITION LOGIC
 // intialise accumulatingValue as 0
@@ -12,9 +10,14 @@ document.getElementById("keyboard").addEventListener("click", func2, false);
 // add currentValue to accumulatingValue when + clicked
 let displayNumb = [];
 function func2(t){
+  console.log(t.target.innerHTML);
+  console.log(typeof t.target.innerHTML);
   if(t.target.innerHTML.charCode > 47 || t.target.innerHTML.charCode < 58){
     console.log("you clicked a number");
     displayNumb.push(t.target.innerHTML);
     console.log(displayNumb);
   }
+  console.log("you didn't click a number");
 }
+
+document.getElementById("keyboard").addEventListener("click", func2, false);
