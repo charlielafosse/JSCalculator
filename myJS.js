4146d3097de33e049734ded8b753a6a9014af626
charlie la fosse <charlielafosse@outlook.com>
2018-10-19 16:23:12 +0100

    test display array

--- myJS.js
+++ myJS.js
@@ -4,4 +4,15 @@ function func(e){
   }
 }
 
-document.getElementById("keyboard").addEventListener("click", func, false);
+document.getElementById("keyboard").addEventListener("click", func2, false);
+
+// addition logic
+// accumulate button clicks into a number
+let displayNumb = [];
+function func2(t){
+  if(t.target.innerHTML.charCode > 47 || e.charCode < 58){
+    console.log("you clicked a number");
+    displayNumb.push(t.target.innerHTML);
+    console.log(displayNumb);
+  }
+}
